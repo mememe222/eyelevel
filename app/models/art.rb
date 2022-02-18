7e261563a65f5638b503695fd475534b280dc599
mememe222 <mememe@hamabenaotonoiMac.local>
2022-02-18 18:32:46 +0900

    複数枚投稿完了

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,5 +1,5 @@
 class Art < ApplicationRecord
   has_many_attached :images
   validates :title, presence: true
-  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 end
